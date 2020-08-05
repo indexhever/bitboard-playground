47cd4cac23b3bfca95daf110e379f1698b84a9bb
Heverton Sarah <heverton.sarah@gmail.com>
2020-08-05 15:32:56 -0300

    Initial work

---
+++ Assets/Scripts/BitBoard.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace BitPlayground
+{
+    public class BitBoard
+    {
+        private int height;
+        private int width;
+
+        public BitBoard(int rowAmount, int columnAmount)
+        {
+            this.height = rowAmount;
+            this.width = columnAmount;
+        }
+
+        public long SetState(long bitMask, int row, int column)
+        {
+            int cellPosition = GetCellPositionByRowAndColumn(row, column);
+            long newBit = 1L << cellPosition;
+            return (bitMask |= newBit);
+        }
+
+        public bool GetCellState(long bitMask, int row, int column)
+        {
+            int cellPosition = GetCellPositionByRowAndColumn(row, column);
+            long searchMask = 1L << cellPosition;
+
+            return IsSearchedMaskInBitMask(searchMask, bitMask);
+        }
+
+        private bool IsSearchedMaskInBitMask(long searchMask, long bitMask)
+        {
+            return ((bitMask & searchMask) != 0);
+        }
+
+        private int GetCellPositionByRowAndColumn(int row, int column)
+        {
+            return (row * width + column);
+        }
+
+        public long ShiftLeft(long bitMask, int offset)
+        {
+            return bitMask >> 1;
+        }
+    }
+}
---
+++ Assets/Scripts/Tests/BitboardOperationsTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using BitPlayground;
+
+namespace Tests
+{
+    public class BitboardOperationsTests
+    {
+        [Test]
+        public void SetStateAtARowAndColumn()
+        {
+            int rowAmount = 2;
+            int columnAmount = 2;
+            BitBoard bitboard = CreateBitBoard(rowAmount, columnAmount);
+            long bitMask = 0;
+            int row = 0;
+            int column = 1;
+            long expectedBitMask = 1L << (row * columnAmount + column);
+
+            bitMask = bitboard.SetState(bitMask, row, column);
+
+            Assert.AreEqual(expectedBitMask, bitMask);
+        }
+
+        [Test]
+        public void GetCellState()
+        {
+            int rowAmount = 2;
+            int columnAmount = 2;
+            BitBoard bitboard = CreateBitBoard(rowAmount, columnAmount);
+            long bitMask = 1L << 1;
+            int row = 0;
+            int column = 1;
+
+            bool hasValueBitValueOfOneInSearchedPosition = bitboard.GetCellState(bitMask, row, column);
+
+            Assert.IsTrue(hasValueBitValueOfOneInSearchedPosition);
+        }
+
+        [Test]
+        public void ShiftLeft()
+        {
+            int rowAmount = 3;
+            int columnAmount = 3;
+            BitBoard bitboard = CreateBitBoard(rowAmount, columnAmount);
+            long bitMask = 0;
+            bitMask = bitboard.SetState(bitMask, 0, 0);
+            bitMask = bitboard.SetState(bitMask, 0, 1);
+            bitMask = bitboard.SetState(bitMask, 1, 1);
+            bitMask = bitboard.SetState(bitMask, 1, 2);
+            string bitMaskString = Convert.ToString(bitMask, 2).PadLeft(9, '0');
+
+            long shiftedBitMask = bitboard.ShiftLeft(bitMask, 1);
+            string shiftedBitMaskString = Convert.ToString(shiftedBitMask, 2).PadLeft(9, '0');
+
+            Debug.Log(shiftedBitMaskString);
+
+            Assert.AreEqual("000110011", bitMaskString);
+            Assert.AreEqual("000011001", shiftedBitMaskString);
+        }
+
+        private BitBoard CreateBitBoard(int rowAmount, int columnAmount)
+        {
+            return new BitBoard(rowAmount, columnAmount);
+        }
+    }
+}
